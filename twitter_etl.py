3f066a74cb851060c0c75f7aee198023adb47b6c
Rahul <rahul@liquidanalytics.com>
2023-08-27 20:35:13 -0400

    Adding the updated code

--- twitter_etl.py
+++ twitter_etl.py
@@ -3,41 +3,44 @@ import pandas as pd
 import json
 from datetime import datetime
 
-access_key = ""
-access_secret = ""
-consumer_key = ""
-consumer_secret = ""
 
-# Twitter authentication
+def run_twitter_et():
 
-auth = tweepy.OAuthHandler(access_key, access_secret)
-auth.set_access_token(consumer_key,consumer_secret)
+    access_key = ""
+    access_secret = ""
+    consumer_key = ""
+    consumer_secret = ""
 
-# Creating an api object
-api = tweepy.API(auth)
+    # Twitter authentication
 
-# screen_name is used to get a particular user
-# count get the number of tweets, 200 is max
-# rts means retweets so we kept it as False
-#
-tweets = api.user_timeline(screen_name='@elonmusk', count=200, include_rts = False, tweet_mode = 'extended')
+    auth = tweepy.OAuthHandler(access_key, access_secret)
+    auth.set_access_token(consumer_key,consumer_secret)
 
-# check the above code is working
-# print(tweets)
-# we get the JSON data so need transformation
+    # Creating an api object
+    api = tweepy.API(auth)
 
-tweet_list = []
+    # screen_name is used to get a particular user
+    # count get the number of tweets, 200 is max
+    # rts means retweets so we kept it as False
+    #
+    tweets = api.user_timeline(screen_name='@elonmusk', count=200, include_rts = False, tweet_mode = 'extended')
 
-for tweet in tweets:
-    text = tweet._json["full_text"]
+    # check the above code is working
+    # print(tweets)
+    # we get the JSON data so need transformation
 
-    refined_tweet = {"user": tweet.user.screen_name,
-                    'text': text,
-                    'favorite_count': tweet.favorite_count,
-                    'retweet_count': tweet.retweet_count,
-                    'created_at': tweet.created_at}
+    tweet_list = []
 
-    tweet_list.append(tweet_list)
+    for tweet in tweets:
+        text = tweet._json["full_text"]
 
-df = pd.DataFrame(tweet_list)
-df.to_csv("elonmusk_twitter_data.csv")
+        refined_tweet = {"user": tweet.user.screen_name,
+                        'text': text,
+                        'favorite_count': tweet.favorite_count,
+                        'retweet_count': tweet.retweet_count,
+                        'created_at': tweet.created_at}
+
+        tweet_list.append(tweet_list)
+
+    df = pd.DataFrame(tweet_list)
+    df.to_csv("elonmusk_twitter_data.csv")
